c75c4d8f8925f3f08708ee5f550a93aabe8fbcc0
Christina Deemer <cahdeemer@gmail.com>
2015-08-17 14:10:18 -0400

    refactored; created getColor, getRadius

--- cultural-fund-map/main.js
+++ cultural-fund-map/main.js
@@ -7,38 +7,46 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
     accessToken: 'pk.eyJ1IjoiY2FoZGVlbWVyIiwiYSI6ImIwYzM3OGYyZTBmYmZlMWJmMzQ5OTNmZWRjMTA3NjNmIn0.4lMKKySybu846ym7-BNbYA'
 }).addTo(map);
 
-var dataSuccess = function( jsonData ) {
-	console.log(jsonData);
-	var layerOptions = {
-		pointToLayer: function(featureData, latlng) {
-			var markerOptions = {
-				color: '#f1f2f2',
-				fillOpacity: 0.9,
-				fillColor: (function(x) {
-					if (x > 49999)
-					return '#002C35';
-					if (x > 24999)
-					return '#006e85';
-					if (x > 14999)
-					return '#4d9aaa'
-					else
-					return '#99c5c3';
-				}
-				(featureData.properties.grantAmount)),
-				radius: (function(x) {
-					if (x > 1500000)
-					return 15;
-					if (x > 400000)
-					return 12;
-					if (x >150000)
-					return 9;
-					if (x > 50000)
-					return 6
-					else 
-					return 3;
-				}
-				(featureData.properties.orgBudget))
+var dataSuccess = function(jsonData) {
+    console.log(jsonData);
+    var layerOptions = {
+        pointToLayer: function(featureData, latlng) {
+
+            var getColor = function(x) {
+            var color = '#99c5c3';
+
+			if (x > 49999) {
+			  color = '#002C35';
+			} else if (x > 24999) {
+			  color = '#006e85';
+			} else if (x > 14999) {
+			  color = '#4d9aaa';
 			}
+
+			return color;
+			},
+
+			getRadius = function(x) {
+			var radius = 3;
+
+			if (x > 1500000) {
+			  radius = 15;
+			} else if (x > 400000) {
+			  radius = 12;
+			} else if (x > 150000) {
+			  radius = 9;
+			} 
+
+			return radius;
+			},
+
+			markerOptions = {
+				color: '#f1f2f2',
+				fillOpacity: 0.8,
+				fillColor: getColor(featureData.properties.grantAmount),
+				radius: getRadius(featureData.properties.orgBudget)
+			};
+
 			return L.circleMarker(latlng, markerOptions);
 		}
 	};
